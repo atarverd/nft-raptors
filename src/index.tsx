8d320f2cc2c770b7562dfa343780a6e9842bb133
robbal <robert.baloyan@gmail.com>
2022-11-14 20:42:53 +0400

    draft-nft-page

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,18 @@
-import { ColorModeScript } from "@chakra-ui/react"
-import * as React from "react"
-import * as ReactDOM from "react-dom/client"
-import { App } from "./App"
+import { ColorModeScript } from "@chakra-ui/react";
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+import { App } from "./App";
+import { BrowserRouter as Router } from "react-router-dom";
 
-
-
-const container = document.getElementById("root")
-if (!container) throw new Error('Failed to find the root element');
-const root = ReactDOM.createRoot(container)
+const container = document.getElementById("root");
+if (!container) throw new Error("Failed to find the root element");
+const root = ReactDOM.createRoot(container);
 
 root.render(
-    <>
+  <>
+    <Router>
       <ColorModeScript />
       <App />
-    </>
-)
-
+    </Router>
+  </>
+);
