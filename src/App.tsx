e11cc11aa0858185b5e809dbb952cb540545eddf
atarverd <artarwho@gmail.com>
2022-11-15 19:59:51 +0400

    fix:(nft page icons/chart)

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import * as React from "react";
 import { ChakraProvider, theme } from "@chakra-ui/react";
-import { ColorModeSwitcher } from "./ColorModeSwitcher";
 import { Routes, Route } from "react-router";
 import SignUp from "./components/auth/signUp";
 import LogIn from "./components/auth/logIn";
--- src/components/nftPage/NftAccordian.tsx
+++ src/components/nftPage/NftAccordian.tsx
@@ -1,12 +1,14 @@
 import React, { FC } from "react";
 import {
   Box,
+  Flex,
   Accordion,
   AccordionItem,
   AccordionButton,
   AccordionIcon,
   AccordionPanel,
 } from "@chakra-ui/react";
+import {FaChartLine,FaHistory,FaTh} from "react-icons/fa"
 import { type } from "@testing-library/user-event/dist/types/setup/directApi";
 
 type FCProps = {
@@ -21,9 +23,10 @@ const NftAccordion = ({ children, accordionName }: FCProps) => {
         <AccordionItem>
           <h2>
             <AccordionButton>
-              <Box flex='1' textAlign='left'>
-                {accordionName}
-              </Box>
+              <Flex flex='1' textAlign='left'>
+                <Box m='5px'>{accordionName==='History'?<FaHistory/> : accordionName.includes('More')?<FaTh/>:<FaChartLine/>}</Box>
+                <Box m='5px'>{accordionName}</Box>
+              </Flex>
               <AccordionIcon />
             </AccordionButton>
           </h2>
