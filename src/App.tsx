9dd996e5c9dab603bf5f84b7c226ad03ba217c10
atarverd <artarwho@gmail.com>
2022-11-15 18:04:51 +0400

    fix:(fixed App.tsx)

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,21 @@ import { ColorModeSwitcher } from "./ColorModeSwitcher";
 import { Routes, Route } from "react-router";
 import SignUp from "./components/auth/signUp";
 import LogIn from "./components/auth/logIn";
-import { Routes, Route } from "react-router";
 import NftPage from "./pages/nftPage";
+import Main from "./pages/main";
+import UserPage from "./pages/userPage";
+import CollectionPage from "./pages/collectionPage";
 
 
 export const App = () => (
   <ChakraProvider theme={theme}>
     <Routes>
+      <Route path='/' element={<Main/>}/>
+      <Route path='/:id' element={<UserPage/>}/>
+      <Route path='collection/:id' element={<CollectionPage/>}/>
       <Route path='/login' element={<LogIn />} />
       <Route path='/signup' element={<SignUp />} />
-      <Route path='/' element={<NftPage />} />
+      <Route path='/nft/:id' element={<NftPage />} />
     </Routes>
   </ChakraProvider>
 );
