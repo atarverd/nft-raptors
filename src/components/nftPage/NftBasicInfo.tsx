b213c132eb4505eedaaa061bfbb65ef4d902f5ba
Rafaye77 <rafayelghazarian@gmail.com>
2022-11-16 18:47:40 +0400

    fix:(navigation, searchButton)

--- src/components/nftPage/NftBasicInfo.tsx
+++ src/components/nftPage/NftBasicInfo.tsx
@@ -1,13 +1,30 @@
 import React from "react";
 import { Box, Flex, Text, Link, Button } from "@chakra-ui/react";
 import { FaRegHeart } from "react-icons/fa";
+import { useNavigate } from "react-router";
 
 const NftBasicInfo = () => {
+  const navigate = useNavigate();
+
+  const navigateToUser = (nameID: string) => {
+    navigate("/" + nameID);
+  };
+
+  const navigateToCollection = (collectionID: string) => {
+    navigate("/collection/" + collectionID);
+  };
+
   return (
     <>
       <Box w='500px' h='120px'>
         <Flex flexDirection='column'>
-          <Link _hover={{ textDecoration: "none" }} color='#2081e2'>
+          <Link
+            onClick={() => {
+              navigateToCollection("collectionID");
+            }}
+            _hover={{ textDecoration: "none" }}
+            color='#2081e2'
+          >
             Collection Name
           </Link>
           <Text as='b' fontSize='2xl'>
@@ -15,7 +32,13 @@ const NftBasicInfo = () => {
           </Text>
           <Text>
             Owned by &nbsp;
-            <Link _hover={{ textDecoration: "none" }} color='#2081e2'>
+            <Link
+              onClick={() => {
+                navigateToUser("nameID");
+              }}
+              _hover={{ textDecoration: "none" }}
+              color='#2081e2'
+            >
               Name
             </Link>
           </Text>
