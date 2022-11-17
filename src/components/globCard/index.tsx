3fcc349e1b91feca08a223e492f5746a0b33fa3e
Rafaye77 <rafayelghazarian@gmail.com>
2022-11-17 16:57:00 +0400

    fix: (fixed flex)

--- src/components/globCard/index.tsx
+++ src/components/globCard/index.tsx
@@ -1,12 +1,5 @@
-import signup from '../../assets/signup.jpg';
-import {
-  Box,
-  Text,
-  Stack,
-  Image,
-  Button,
-  Heading
-} from '@chakra-ui/react';
+import signup from "../../assets/signup.jpg";
+import { Box, Text, Stack, Image, Button, Heading } from "@chakra-ui/react";
 
 const GlobCard = () => {
   return (
@@ -15,21 +8,16 @@ const GlobCard = () => {
         maxW='200px'
         borderRadius='8px'
         overflow='hidden'
-        boxShadow='0 0 24px 4px rgba(0, 0, 0, 0.15)'>
-        <Image
-          src={signup}
-          w='200px'
-          h='220px'
-          borderRadius='5px' />
-        <Stack p='2' bg='#EBF8FF'>
-          <Heading fontSize='2xl'>
-            Card
-          </Heading>
+        boxShadow='0 0 24px 4px rgba(0, 0, 0, 0.15)'
+      >
+        <Image src={signup} w='200px' h='220px' borderRadius='5px' />
+        <Stack p='3' bg='#EBF8FF'>
+          <Heading fontSize='2xl'>Card</Heading>
           <Text>Name: AMD</Text>
           <Button colorScheme='messenger'>Add To Cart</Button>
         </Stack>
-      </Box >
-    </Box >
+      </Box>
+    </Box>
   );
 };
 
--- src/components/searchPage/index.tsx
+++ src/components/searchPage/index.tsx
@@ -1,28 +1,25 @@
-import ItemCard from './itemCard'
-import Accordions from './accordion'
-import {
-  Box,
-  Flex
-} from '@chakra-ui/react'
+import ItemCard from "./itemCard";
+import Accordions from "./accordion";
+import { Box, Flex } from "@chakra-ui/react";
 
 const Search = () => {
   return (
-    <Box>
-      <Flex display='flex' justifyContent='space-between'>
+    <Box m='15px'>
+      <Flex display='flex' justifyContent='space-around'>
         <Box>
-          <Flex display='flex' >
+          <Flex display='flex'>
             <Accordions />
           </Flex>
         </Box>
 
-        <Box m='15px'>
+        <Box>
           <Flex display='flex'>
             <ItemCard />
           </Flex>
         </Box>
       </Flex>
     </Box>
-  )
-}
+  );
+};
 
-export default Search
+export default Search;
