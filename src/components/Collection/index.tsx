06413c65e560462e7a5b812410a7c0167e6a0bcb
Rafaye77 <rafayelghazarian@gmail.com>
2022-11-22 17:21:48 +0400

    feat:(navigation to NFT Page)

--- src/components/Collection/index.tsx
+++ src/components/Collection/index.tsx
@@ -1,55 +1,57 @@
-import GlobCard from '../globCard'
-import CollectionHeader from './collectionHeader'
+import GlobCard from "../globCard";
+import CollectionHeader from "./collectionHeader";
 
-import {
-  Box,
-  Flex,
-  SimpleGrid
-} from '@chakra-ui/react'
+import { Box, Flex, SimpleGrid } from "@chakra-ui/react";
 import { collection, query, where, getDocs } from "firebase/firestore";
-import { useParams } from 'react-router';
-import  { useEffect, useState } from "react";
-import {db} from '../../firebase-config.js'
+import { useParams } from "react-router";
+import { useEffect, useState } from "react";
+import { db } from "../../firebase-config.js";
 
-type TNft={
-  img:string;
-  name:string;
-  currentPrice:number;
-}
+type TNft = {
+  id: string;
+  img: string;
+  name: string;
+  currentPrice: number;
+};
 
-const Collection = () => {  
-  const { id } = useParams()
-  const [nfts,setNfts]=useState<TNft[]>()
-  useEffect(()=>{
-    const a=async () => {
+const Collection = () => {
+  const { id } = useParams();
+  const [nfts, setNfts] = useState<TNft[]>();
+  useEffect(() => {
+    const a = async () => {
       const q = query(collection(db, "nfts"), where("collectionID", "==", id));
 
       const querySnapshot = await getDocs(q);
       //@ts-ignore
-      const result=[]
+      const result = [];
       querySnapshot.forEach((doc) => {
         // doc.data() is never undefined for query doc snapshots
         console.log(doc.id, " => ", doc.data());
-        const nft=doc.data()
-        result.push({img:nft.img,name:nft.name,currentPrice:nft.currentPrice})
+        const nft = doc.data();
+        result.push({
+          id: doc.id,
+          img: nft.img,
+          name: nft.name,
+          currentPrice: nft.currentPrice,
+        });
       });
       //@ts-ignore
-      setNfts(result)
-    }
-    a()
-    
-  },[])
+      setNfts(result);
+    };
+    a();
+  }, []);
   return (
     <Box>
       <CollectionHeader />
       <Flex display='flex' justifyContent='space-around'>
         <SimpleGrid spacing='40px' columns={5} m='20px'>
-          {nfts?.map(item=><GlobCard nft={item} />)}
-          
+          {nfts?.map((item) => (
+            <GlobCard nft={item} />
+          ))}
         </SimpleGrid>
       </Flex>
     </Box>
-  )
-}
+  );
+};
 
-export default Collection
+export default Collection;
--- src/components/globCard/index.tsx
+++ src/components/globCard/index.tsx
@@ -1,15 +1,23 @@
 import signup from "../../assets/signup.jpg";
 import { Box, Text, Stack, Image, Button, Heading } from "@chakra-ui/react";
+import { useNavigate } from "react-router-dom";
 
-type TNft={
-  nft:{
-    img:string;
-  name:string;
-  currentPrice:number;
-  }
-}
+type TNft = {
+  nft: {
+    id: string;
+    img: string;
+    name: string;
+    currentPrice: number;
+  };
+};
 
-const GlobCard = ({nft}:TNft) => {
+const GlobCard = ({ nft }: TNft) => {
+  const navigate = useNavigate();
+
+  const toNftPage = () => {
+    navigate("/nft/" + nft.id);
+  };
+
   return (
     <Box>
       <Box
@@ -18,17 +26,20 @@ const GlobCard = ({nft}:TNft) => {
         overflow='hidden'
         boxShadow='0 0 24px 4px rgba(0, 0, 0, 0.15)'
       >
-
         <Image
+          onClick={toNftPage}
           src={nft.img}
           w='200px'
           h='220px'
           borderRadius='5px'
           transition='transform .2s;'
-          _hover={{ 'transform': 'scale(1.1)' }} />
+          _hover={{ transform: "scale(1.1)" }}
+        />
 
         <Stack p='3' bg='#EBF8FF'>
-          <Heading fontSize='2xl'>{nft.name}</Heading>
+          <Heading onClick={toNftPage} fontSize='2xl'>
+            {nft.name}
+          </Heading>
           <Text>price: {nft.currentPrice}</Text>
           <Button colorScheme='messenger'>Add To Cart</Button>
         </Stack>
