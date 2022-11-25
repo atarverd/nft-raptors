7446e2e3d2cc3b7c6000cb87eb23a2c8e7630291
Rafaye77 <rafayelghazarian@gmail.com>
2022-11-25 18:39:55 +0400

    auth logic

--- src/components/createCollection/index.tsx
+++ src/components/createCollection/index.tsx
@@ -6,32 +6,24 @@ import {
   Input,
   FormControl,
   FormLabel,
-} from '@chakra-ui/react'
-import DragDrop from './dragDrop'
+} from "@chakra-ui/react";
 
 const CreateCollection = () => {
   return (
     <Box>
-
       <Text fontSize='5xl'>Create a Collection</Text>
       <Box>
         <FormControl isRequired>
           <FormLabel fontSize='3xl'>Logo Image</FormLabel>
           <Text>This image will also be used for navigation</Text>
         </FormControl>
-        <DragDrop />
       </Box>
 
-      <Box>
+      <Box></Box>
 
-      </Box>
-
-      <Box>
-
-      </Box>
-
+      <Box></Box>
     </Box>
-  )
-}
+  );
+};
 
-export default CreateCollection
+export default CreateCollection;
--- src/firebase-config.js
+++ src/firebase-config.js
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -11,9 +12,10 @@ const firebaseConfig = {
   projectId: "nft-raptors",
   storageBucket: "nft-raptors.appspot.com",
   messagingSenderId: "1077461821090",
-  appId: "1:1077461821090:web:5ea8a3042e0612ccbe99fb"
+  appId: "1:1077461821090:web:5ea8a3042e0612ccbe99fb",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app)
+export const auth = getAuth(app);
+export const db = getFirestore(app);
